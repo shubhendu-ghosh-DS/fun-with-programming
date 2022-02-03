cc9d87ff91083778cc90c66b2d757229b0bfca82
shubhendu ghosh <77840111+shubhendu-ghosh-DS@users.noreply.github.com>
2022-02-03 12:50:13 +0530

    Update unique-dates.jl

--- unique-dates.jl
+++ unique-dates.jl
@@ -12,3 +12,27 @@ end
 
 
 
+
+function zeroDD(x)
+    if x<10
+        val = "0$(x)"
+    else
+        val = "$(x)"
+    end
+    return val
+end
+
+
+function leastdigit()
+    for i in 2000:3000
+        for j in 1:12
+            for k in 1:31
+                s = "$(zeroDD(k))"*"$(zeroDD(j))"*"$(i)"
+                s1 = "$(zeroDD(k))/"*"$(zeroDD(j))/"*"$(i)"
+                if uniqchar(s) == 2
+                    println(s1)
+                end
+            end
+        end
+    end
+end
