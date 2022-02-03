a13856779a9f94c0da2f7682f56b82ed0dd9376e
shubhendu ghosh <77840111+shubhendu-ghosh-DS@users.noreply.github.com>
2022-02-03 12:29:38 +0530

    Update unique-dates.jl

--- unique-dates.jl
+++ unique-dates.jl
@@ -1,1 +1,18 @@
-function
+function uniqchar(x)
+    D =[]
+    for i in x
+        if i ∈ D
+            continue
+        else
+            append!(D, i)
+        end
+    end
+    return length(D)
+end
+
+
+
+function datecount(x)
+    y = string(x)
+    return uniqchar(y)
+end
