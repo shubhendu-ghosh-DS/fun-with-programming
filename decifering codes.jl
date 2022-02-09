18cd2daddbab697c6eb154d78baea44b9d472cc9
shubhendu ghosh <77840111+shubhendu-ghosh-DS@users.noreply.github.com>
2022-02-09 16:11:37 +0530

    Add files via upload

---
+++ decifering codes.jl
@@ -0,0 +1,141 @@
+using Random
+
+A1 = "qwertyuiopasdfghjklzxcvbnm"
+B = [",",".",";",":","?","-","_","!","@","#","%","&","(",")"]
+C = ["@","#","&","_"]
+
+A = []
+for j in A1
+    push!(A, j)
+end
+
+
+D = Dict()
+num = [2,3,4,5,6,7]
+
+function generate(x)
+    vec = []
+    for j in split(x)
+        push!(vec, j)
+    end
+    for k in vec
+        if k ∈ keys(D)
+            continue
+        elseif k ∈ B
+            q = rand([2,3,4])
+            code = times(C,q)
+        else
+            p = rand(num)
+            code = times(A,p)
+        end
+        D["$(k)"] = "$(code)"
+    end
+end
+        
+            
+    
+    
+
+function times(A,n)
+    if n==2
+        return rand(A)*rand(A)
+    elseif n==3
+        return rand(A)*rand(A)*rand(A)
+    elseif n==4
+        return rand(A)*rand(A)*rand(A)*rand(A)
+    elseif n==5
+        return rand(A)*rand(A)*rand(A)*rand(A)*rand(A)
+    elseif n == 6
+        return rand(A)*rand(A)*rand(A)*rand(A)*rand(A)*rand(A)
+    else
+        return rand(A)*rand(A)*rand(A)*rand(A)*rand(A)*rand(A)*rand(A)
+    end
+end
+
+generate("we are in this time loop forever")
+
+function codefo(x)
+    sos = []
+    state = ""
+    for jin in split(x)
+        push!(sos, jin)
+    end
+    for kim in sos
+        if kim ∈ keys(D)
+            state = state*D[kim]*" "
+        else
+            generate(kim)
+            codefo(x)
+        end
+    end
+    #println("If your output has @ sign then there is no code for that word in our database. please assign a code for that word")
+    return state[1:end-1]
+end
+
+function reverse_code(x)
+    rev = ""
+    for kol in split(x)
+        if kol ∈ values(D)
+            for i in keys(D)
+                if D[i] == kol
+                    rev = rev*i*" "
+                end
+            end
+        else
+            rev = rev*"__ "
+        end
+    end
+    return rev[1:end-1]
+end
+
+reverse_code("nep")
+
+function codefor(x)
+    s = codefo(x)
+    s = codefo(x)
+    return s
+end
+
+
+function encrypt(x)
+    str = ""
+    for i in x
+        str = str*" "*i
+    end
+    str = remove_space(codefor(str),2)
+    return str
+end
+
+function remove_space(x, p = 1)
+    state = ""
+    if p == 1
+        for n in split(x)
+            state = state*n*""
+        end
+    else
+        for n in split(x)
+            state = state*n*"%"
+        end
+    end
+    return state
+end
+
+function add_space(x)
+    str = ""
+    for i in x
+        str = str*i*" "
+    end
+    return str
+end
+
+function decrypt(x)
+    vet = split(x, "%")
+    vet = add_space(vet)
+    mes = reverse_code(vet)
+    mes = remove_space(mes)
+    return mes
+end
+
+D
+
+
