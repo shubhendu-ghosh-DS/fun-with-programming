275352b367d1937695329c086bf1835319713bc0
shubhendu ghosh <77840111+shubhendu-ghosh-DS@users.noreply.github.com>
2022-04-30 12:47:05 +0530

    Update embed_binary.py

--- embed_binary.py
+++ embed_binary.py
@@ -1,1 +1,12 @@
+word = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z, "
+s = word.split(",")
+word = "".join(s)
+words = []
+for ele in word:
+    words.append(ele)
+assign = []
+for i in range(1,28):
+    assign.append(i)
+    
+ 
 
